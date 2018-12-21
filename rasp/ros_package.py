da3b2bdef6d740353e44b2a46e6eef33a50dcbbc
Gabriel <gabriel.moneyron@yahoo.fr>
2018-12-21 15:57:19 +0100

    create file common to virtual robot and raspberry

---
+++ rasp/ros_package.py
@@ -0,0 +1,29 @@
+import rospy
+import cv_bridge
+from threading import Thread
+from std_msgs.msg import String
+from sensor_msgs.msg import CompressedImage
+
+
+class RosNodeRaspberry(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        print("Starting ROS node")
+        rospy.init_node('virtualbot', anonymous=True)
+        rospy.Subscriber('commands', String, self.__callback)
+        self.__image_publisher = rospy.Publisher("video_frame", CompressedImage, queue_size=1)
+        self.__bridge = cv_bridge.CvBridge()
+        self.__command = ""
+
+    def publish_image(self, image):
+        print("Send image")
+        self.__image_publisher.publish(self.__bridge.cv2_to_compressed_imgmsg(image))
+
+    def __callback(self, data):
+        rospy.loginfo(rospy.get_caller_id() + ' received ' + data.data)
+        self.__command = data.data
+
+    def __get_command(self):
+        return self.__command
+
+    command = property(__get_command)
