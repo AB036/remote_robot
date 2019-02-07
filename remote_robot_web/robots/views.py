a9e30afc30e32a56e1c7dcc85d6681f9eda32e6a
SylvestreSakti <sylvestre.prabakaran@gmail.com>
2019-02-07 20:49:07 +0100

    Switched on cam

--- remote_robot_web/robots/views.py
+++ remote_robot_web/robots/views.py
@@ -41,13 +41,16 @@ class StreamingVideoView(View):
         i = 0
         while True:
             i+=1
-            print(i)
+            #print(i)
             #frame = camera.get_frame()
             ret, image = self.video.read()
 
             imageRec = SocketConnection.frame
-            imageRec = np.random.randint(0,255,(480,640,3))
+            print(np.shape(image))
+            print(np.shape(imageRec))
             ret1, jpeg1 = cv2.imencode('.jpg', image)
             ret, jpeg = cv2.imencode('.jpg', imageRec)
+            #print(np.shape(jpeg))
+            #print(np.shape(jpeg1))
             frame = jpeg.tobytes()
             yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
