5642b3d1f1d7eb3c7246e793eae9afb304c6c04f
SylvestreSakti <sylvestre.prabakaran@gmail.com>
2019-02-07 18:56:08 +0100

    Switched on the webcam

--- remote_robot_web/robots/views.py
+++ remote_robot_web/robots/views.py
@@ -10,15 +10,15 @@ from control_board.socket_connection import SocketConnection
 class StreamingVideoView(View):
     class VideoCamera:
         def __init__(self):
-            #self.video = cv2.VideoCapture(0)
+            self.video = cv2.VideoCapture(0)
             pass
 
         def __del__(self):
-            #self.video.release()
+            self.video.release()
             pass
 
         def get_frame(self):
-            #ret, image = self.video.read()
+            ret, image = self.video.read()
             imageRec = SocketConnection.frame
             ret, jpeg = cv2.imencode('.jpg', imageRec)
             return jpeg.tobytes()
