6b3af1688b1a79aaae126e3b772e708a1d63a89f
SylvestreSakti <sylvestre.prabakaran@gmail.com>
2019-02-12 22:52:45 +0100

    Removed thread from the client

--- remote_robot_web/control_board/views.py
+++ remote_robot_web/control_board/views.py
@@ -9,12 +9,7 @@ from django.template import loader
 from control_board.socket_connection import SocketConnection
 
 
-socket_thread = SocketConnection()  # Creates the socket thread to connect in localhost with ROS
-
-
 def index(request):
-    if not socket_thread.is_alive():
-        socket_thread.start()
     template = loader.get_template('control_board/index.html')
     return HttpResponse(template.render(request=request))
 
@@ -23,13 +18,13 @@ def move(request):
     direction = request.GET.get('direction', None)
 
     if direction == "up":
-        socket_thread.send_command(0, 0)
+        SocketConnection.send_command(0, 0)
     elif direction == "down":
-        socket_thread.send_command(0, 1)
+        SocketConnection.send_command(0, 1)
     elif direction == "left":
-        socket_thread.send_command(0, 2)
+        SocketConnection.send_command(0, 2)
     elif direction == "right":
-        socket_thread.send_command(0, 3)
+        SocketConnection.send_command(0, 3)
 
     return JsonResponse({'direction': direction})
 
