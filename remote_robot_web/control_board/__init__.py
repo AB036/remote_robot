35861b10575773e125094595613fc6d369eb7157
SylvestreSakti <sylvestre.prabakaran@gmail.com>
2019-02-12 22:53:31 +0100

    Launch the thread while initializing the server

--- remote_robot_web/control_board/__init__.py
+++ remote_robot_web/control_board/__init__.py
@@ -1,2 +1,5 @@
 from control_board.socket_connection import SocketConnection
 
+socket_thread = SocketConnection()  # Creates the socket thread to connect in localhost with ROS
+if not socket_thread.is_alive():
+    socket_thread.start()
