599c24fa8322e60868befc123075fe9fbc83e38f
AB036 <43874999+AB036@users.noreply.github.com>
2019-02-17 16:57:06 +0100

    Update ros_server.py
    More comments

--- remote_robot_ros/remote_robot/scripts/ros_server.py
+++ remote_robot_ros/remote_robot/scripts/ros_server.py
@@ -12,12 +12,12 @@ from robot_communication import RobotComm
 
 def ros_server():
 	global local_comm
-	#connect to the django script via localhost
+	# connect to the django script via localhost
 	local_comm = LocalComm(12800)
 	local_comm.start()
 	print("local comm opened")
 	
-	#ros node to communicate with the robot
+	# ros node to communicate with the robot
 	rospy.init_node('ros_server')
 	pub = rospy.Publisher('commands', String, queue_size=10)
 	robot0 = RobotComm(0, pub, local_comm, 640, 480)
@@ -26,6 +26,7 @@ def ros_server():
 	print("robot0 initialized")
 
 	while not rospy.is_shutdown():
+		# do nothing while the other thread is running
 		rospy.sleep(0.05)
 	
 	rospy.spin()
@@ -36,8 +37,9 @@ if __name__ == '__main__':
     try:
         ros_server()
     except rospy.ROSInterruptException:
+	# this exception is triggered when the application is stopped by CTRL+C
         pass
 
-
+# shut down the other thread by raising an event
 local_comm.stop()
     
