526e2589c567270158309ad3f744d045baf13eaf
AB036 <43874999+AB036@users.noreply.github.com>
2019-02-17 17:34:35 +0100

    More comments

--- remote_robot_ros/remote_robot/scripts/local_communication.py
+++ remote_robot_ros/remote_robot/scripts/local_communication.py
@@ -7,6 +7,10 @@ import time
 
 
 class LocalComm(threading.Thread):
+	"""
+	This class is used to communicate with the webserver via a localhost socket
+	port (int): the port used by the socket. Must be the same value as the port used by the webserver  
+	"""
 	def __init__(self, port):
 		if type(port) != int:
 			raise TypeError("port should be an int")
@@ -19,10 +23,10 @@ class LocalComm(threading.Thread):
 		self.__connect()
 		
 	def __connect(self):
+		# Connect to the webserver via localhost
 		try:
 			self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			self.__socket.settimeout(10)
-			#self.__socket.connect(('138.195.207.184', self.__PORT))
 			self.__socket.connect(('127.0.0.1', self.__PORT))
 			self.__socket.sendall(chr(25))
 			data = self.__socket.recv(1024)
@@ -31,15 +35,23 @@ class LocalComm(threading.Thread):
 			print("Could not connect to the local server: " + str(e))
 			exit()
 		
-	def add_robot(self, robot_id, robot, video_width, video_heigth):
+	def add_robot(self, robot_id, robot, video_width, video_height):
+		# Add a robot to the instance container and send info to the webserver
+		if type(robot_id) != int:
+			raise TypeError("robot_id should be an int")
+		if type(video_width) != int or type(video_height) != int:
+                        raise TypeError("video_width and video_heigth should be int values")
 		self.__robots[robot_id] = robot
 		try:
-			message = chr(01) + chr(robot_id) + chr(video_heigth//256) + chr(video_heigth%256) + chr(video_width//256) + chr(video_width%256)
+			message = chr(01) + chr(robot_id) + chr(video_height//256) + chr(video_height%256) + chr(video_width//256) + chr(video_width%256)
 			self.__socket.sendall(message)
 		except:
 			pass
 		
 	def send_video_frame(self, robot_id, frame):
+		# send a video frame to the webserver
+		if robot_id not in self.__robots:
+			raise ValueError("Can't find the provided robot_id")
 		try:
 			self.__socket.sendall(chr(02) + chr(robot_id))
 			h, w, three = frame.shape
@@ -50,19 +62,22 @@ class LocalComm(threading.Thread):
 			print(e)
 		
 	def __command_callback(self, data):
+		# Triggered when a command is received from the webserver
 		robot_id = ord(data[1])
 		cmd_code = ord(data[2])
-		c = {0:'up', 1:'down', 2:'left', 3:'right', 4:'stop'}[cmd_code]
-		self.__robots[robot_id].send_command(c)
+		if cmd_code in (0,1,2,3) and robot_id in self.__robots:
+			c = {0:'up', 1:'down', 2:'left', 3:'right', 4:'stop'}[cmd_code]
+			self.__robots[robot_id].send_command(c)
 		
 	def run(self):
+		# Main thread routine
+		# Waits for a message from its socket and tries again if the socket times out (10 sec)
 		while not self.__end_event.is_set():
 			try:
 				data = self.__socket.recv(4096)
-				#print("Received: " + data)
 				if data:
 					header = ord(data[0])
-					if header == 10:
+					if header == 10 and len(data) >= 3:
 						self.__command_callback(data)
 			except socket.timeout:
 				pass
@@ -71,6 +86,7 @@ class LocalComm(threading.Thread):
 				pass
 
 	def stop(self):
+		# Stops the thread by raising an event
 		self.__end_event.set()
 
 
